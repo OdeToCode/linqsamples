dd91f4088b644a809583e10801d5e425fb4587f5
Scott <scott@OdeToCode.com>
2016-05-27 22:31:00 -0400

    finished m2

---
+++ Features/MyLinq.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.Linq
+{
+    public static class MyLinq
+    {
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            var count = 0;
+            foreach (var item in sequence)
+            {
+                count += 1;
+            }
+            return count;
+        }
+    }
+}
--- Features/Program.cs
+++ Features/Program.cs
@@ -3,24 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using Features.Linq;
 
 namespace Features
 {
     class Program
     {
         static void Main(string[] args)
-        {            
-            Employee[] developers = new Employee[]
+        {
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) =>
+            {
+                int temp = x + y;
+                return temp;
+            };
+            Action<int> write =  x => Console.WriteLine(x);
+
+            write(square(add(3, 5)));
+
+            var developers = new Employee[]
             {
                 new Employee { Id = 1, Name= "Scott" },
                 new Employee { Id = 2, Name= "Chris" }
             };
 
-            List<Employee> sales = new List<Employee>()
+            var sales = new List<Employee>()
             {
                 new Employee { Id = 3, Name = "Alex" }
             };
-            
+
+            var query = developers.Where(e => e.Name.Length == 5)
+                                  .OrderBy(e => e.Name);
+
+            var query2 = from developer in developers
+                         where developer.Name.Length == 5
+                         orderby developer.Name
+                         select developer;
+
+            foreach (var employee in query2)
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }        
+
+        private static bool NameStartsWithS(Employee employee)
+        {
+            return employee.Name.StartsWith("S");
         }
     }
 }
