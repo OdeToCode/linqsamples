05bf322920d8a21f950b98698399ea0f16980a37
Scott <scott@OdeToCode.com>
2016-06-02 14:18:32 -0400

    reset program.cs

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -11,35 +11,7 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            var cars = LoadCarsFromFile("fuel.csv");
-            foreach (var car in cars)
-            {
-                Console.WriteLine($"{car.Name}:{car.City}");
-            }
-        }
-
-        private static List<Car> LoadCarsFromFile(string path)
-        {
-            return
-                File.ReadAllLines(path)
-                .Skip(1)
-                .Where(line => line.Length > 1)
-                .Select(line =>
-                {
-                    var columns = line.Split(',');
-                    var car = new Car
-                    {
-                        Year = int.Parse(columns[0]),
-                        Manufacturer = columns[1],
-                        Name = columns[2],
-                        Displacement = double.Parse(columns[3]),
-                        Cylinders = int.Parse(columns[4]),
-                        City = int.Parse(columns[5]),
-                        Highway = int.Parse(columns[6]),
-                        Combined = int.Parse(columns[7])
-                    };
-                    return car;
-                }).ToList();
-        }
+           
+        }        
     }
 }
