ca3d7efedac0656a5bf82dc5235f97510a3abc9c
Scott <scott@OdeToCode.com>
2016-06-02 13:56:25 -0400

    start m4

---
+++ Cars/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Displacement { get; set; }
+        public int Cylinders { get; set; }
+        public int City { get; set; }
+        public int Highway { get; set; }
+        public int Combined { get; set; }
+    }
+}
